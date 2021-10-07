8bc88099d2645c4349b478812104fc6ac206b3af
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-10-07 17:40:17 +0300

    первая версия

---
+++ src/sktvp20library/SKTVp20Library.java
@@ -0,0 +1,18 @@
+
+package sktvp20library;
+
+import myclasses.App;
+
+public class SKTVp20Library {
+
+   
+    public static void main(String[] args) {
+        
+      App app;
+      app = new App();
+      app.run();
+      
+        System.out.println("");
+    }
+    
+}
